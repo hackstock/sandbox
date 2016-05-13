6e7308550ba008e40b8a563d7ae43d11d2f23e1f
Edward Pie <edward@smsgh.com>
2016-05-13 08:38:04 +0000

    Practiced nested resources

--- rails/blog/app/models/post.rb
+++ rails/blog/app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ActiveRecord::Base
-	validates :title, :presence => true
+	validates :title, :presence => true, :length => {:minimum => 5, :maximum => 10}
+	has_many :comments
 end
--- rails/blog/db/schema.rb
+++ rails/blog/db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160512162520) do
+ActiveRecord::Schema.define(version: 20160512203430) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "author"
+    t.text     "body"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "email"
+  end
+
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id"
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
